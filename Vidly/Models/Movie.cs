f98ffa1de16810b05fe90f659b4060a5203d2269
ioneltuc <ion.tentiuc@isa.utm.md>
2023-10-16 17:34:45 +0300

    Changed movie constraints

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -7,17 +7,12 @@ public class Movie
     public int Id { get; set; }
     
     [Required]
+    [StringLength(255)]
     public string Name { get; set; }
     
     [Required]
     public MovieGenre Genre { get; set; }
-
-    [Required]
     public DateTime ReleasedDate { get; set; }
-    
-    [Required]
     public DateTime DateAdded { get; set; }
-    
-    [Required]
     public int NumberInStock { get; set; }
 }
